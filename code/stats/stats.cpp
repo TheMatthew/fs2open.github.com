TheMatthew/fs2open.github.com

commit 07107ce9b208d6dc8c5a2efcb9122b8180908103

Fix newly introduced format warnings

code/stats/stats.cpp
@@ -43,77 +43,77 @@ void show_stats_label(int stage, int sx, int sy, int dy)
 {
 	switch ( stage ) {
 		case MISSION_STATS:
-			gr_printf_menu(sx,sy,XSTR( "Mission Stats", 114));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Mission Stats", 114));
 			sy += 2*dy;
-			gr_printf_menu(sx,sy,XSTR( "Total kills", 115));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Total kills", 115));
 			sy += 2*dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary weapon shots", 116));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary weapon shots", 116));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary weapon hits", 117));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary weapon hits", 117));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary friendly hits", 118));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hits", 118));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary hit %%", 119));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary hit %%", 119));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary friendly hit %%", 120));
-			sy += 2*dy;
-
-			gr_printf_menu(sx,sy,XSTR( "Secondary weapon shots", 121));
-			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary weapon hits", 122));
-			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary friendly hits", 123));
-			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary hit %%", 124));
-			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary friendly hit %%", 125));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hit %%", 120));
 			sy += 2*dy;
 
-			gr_printf_menu(sx,sy,XSTR( "Assists", 126));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary weapon shots", 121));
+			sy += dy;
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary weapon hits", 122));
+			sy += dy;
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hits", 123));
+			sy += dy;
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary hit %%", 124));
+			sy += dy;
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hit %%", 125));
+			sy += 2*dy;
+
+			gr_printf_menu(sx,sy,"%s", XSTR( "Assists", 126));
 			sy += 2*dy;
 
 			if(Game_mode & GM_MULTIPLAYER){
-				gr_printf_menu(sx,sy,XSTR( "Player Deaths", 127));
+				gr_printf_menu(sx,sy,"%s", XSTR( "Player Deaths", 127));
 				sy += 2*dy;
 
-				gr_printf_menu(sx,sy,XSTR( "Mission score", 1526));
+				gr_printf_menu(sx,sy,"%s", XSTR( "Mission score", 1526));
 			}
 
 
 			break;
 
 		case ALL_TIME_STATS:
-			gr_printf_menu(sx,sy,XSTR( "All Time Stats", 128));
+			gr_printf_menu(sx,sy,"%s", XSTR( "All Time Stats", 128));
 			sy += 2*dy;			
-			gr_printf_menu(sx,sy,XSTR( "Total kills", 115));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Total kills", 115));
 			sy += 2*dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary weapon shots", 116));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary weapon shots", 116));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary weapon hits", 117));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary weapon hits", 117));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary friendly hits", 118));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hits", 118));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary hit %%", 119));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary hit %%", 119));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Primary friendly hit %%", 120));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hit %%", 120));
 			sy += 2*dy;
 
-			gr_printf_menu(sx,sy,XSTR( "Secondary weapon shots", 121));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary weapon shots", 121));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary weapon hits", 122));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary weapon hits", 122));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary friendly hits", 123));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hits", 123));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary hit %%", 124));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary hit %%", 124));
 			sy += dy;
-			gr_printf_menu(sx,sy,XSTR( "Secondary friendly hit %%", 125));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hit %%", 125));
 			sy += 2*dy;			
 
-			gr_printf_menu(sx,sy,XSTR( "Assists", 126));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Assists", 126));
 			sy += 2*dy;
 
 			if(Game_mode & GM_MULTIPLAYER){
-				gr_printf_menu(sx,sy,XSTR( "Score", 1527));
+				gr_printf_menu(sx,sy,"%s", XSTR( "Score", 1527));
 			}
 			break;
 		} // end switch
@@ -138,62 +138,62 @@ void show_stats_numbers(int stage, int sx, int sy, int dy,int add_mission)
 		case MISSION_STATS:
          // mission kills stats
 			sprintf(text,"%d",Active_player->stats.m_kill_count_ok);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
          // mission primary weapon stats
 			sprintf(text,"%u",Active_player->stats.mp_shots_fired);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.mp_shots_hit);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.mp_bonehead_hits);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if(Active_player->stats.mp_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.mp_shots_hit/(float)Active_player->stats.mp_shots_fired);
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if(Active_player->stats.mp_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.mp_bonehead_hits/(float)Active_player->stats.mp_shots_fired);
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
 			// mission secondary weapon stats
 			sprintf(text,"%u",Active_player->stats.ms_shots_fired);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.ms_shots_hit);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.ms_bonehead_hits);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if(Active_player->stats.ms_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.ms_shots_hit/(float)Active_player->stats.ms_shots_fired);
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if(Active_player->stats.ms_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.ms_bonehead_hits/(float)Active_player->stats.ms_shots_fired);
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
 			// mission assists and player rescues (respawns)
 			sprintf(text,"%d",(int)Active_player->stats.m_assists);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
 			if(Game_mode & GM_MULTIPLAYER){
 				sprintf(text,"%d",(int)Active_player->stats.m_player_deaths);
-				gr_printf_menu(sx,sy,text);
+				gr_printf_menu(sx,sy,"%s", text);
 				sy += 2*dy;
 
 				// mission score
@@ -222,57 +222,57 @@ void show_stats_numbers(int stage, int sx, int sy, int dy,int add_mission)
          // mission kills stats
 			sprintf(text,"%d",Active_player->stats.kill_count_ok + add.kill_count_ok);
 			hud_num_make_mono(text, font::get_current_fontnum());
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
          // alltime primary weapon stats
 			sprintf(text,"%u",Active_player->stats.p_shots_fired + add.p_shots_fired);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.p_shots_hit + add.p_shots_hit);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.p_bonehead_hits + add.p_bonehead_hits);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if((Active_player->stats.p_shots_fired + add.p_shots_fired)>0)
 				pct=(float)100.0*((float)(Active_player->stats.p_shots_hit+add.p_shots_hit)/(float)(Active_player->stats.p_shots_fired + add.p_shots_fired));
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if((Active_player->stats.p_bonehead_hits + add.p_bonehead_hits)>0)
 				pct=(float)100.0*((float)(Active_player->stats.p_bonehead_hits+add.p_bonehead_hits)/(float)(Active_player->stats.p_shots_fired + add.p_shots_fired));
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
 			// alltime secondary weapon stats
 			sprintf(text,"%u",Active_player->stats.s_shots_fired + add.s_shots_fired);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.s_shots_hit + add.s_shots_hit);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			sprintf(text,"%u",Active_player->stats.s_bonehead_hits + add.s_bonehead_hits);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if((Active_player->stats.s_shots_fired+add.s_shots_fired)>0)
 				pct=(float)100.0*((float)(Active_player->stats.s_shots_hit + add.s_shots_hit)/(float)(Active_player->stats.s_shots_fired + add.s_shots_fired));
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if((Active_player->stats.s_bonehead_hits + add.s_bonehead_hits)>0)
 				pct=(float)100.0*((float)(Active_player->stats.s_bonehead_hits+add.s_bonehead_hits)/(float)(Active_player->stats.s_shots_fired+add.s_shots_fired));
 			else pct=(float)0.0;
 			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
 			// alltime assists
 			sprintf(text,"%d",(int)Active_player->stats.assists + add.assists);
-			gr_printf_menu(sx,sy,text);
+			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
 			if (Game_mode & GM_MULTIPLAYER) {

cmake/toolchain-clang.cmake
@@ -64,6 +64,8 @@ endif()
 
 set(COMPILER_FLAGS "${COMPILER_FLAGS} ${SANITIZE_FLAGS}")
 
+set(COMPILER_FLAGS "${COMPILER_FLAGS} -Wformat-security")
+
 set(COMPILER_FLAGS "${COMPILER_FLAGS} -Wno-unused-function")
 
 # Dear Clang, please tell us if a function does not return a value since that part of the standard is stupid!

cmake/toolchain-gcc.cmake
@@ -76,6 +76,8 @@ endif()
 
 set(COMPILER_FLAGS "${COMPILER_FLAGS} ${SANITIZE_FLAGS}")
 
+set(COMPILER_FLAGS "${COMPILER_FLAGS} -Wformat-security")
+
 set(COMPILER_FLAGS "${COMPILER_FLAGS} -Wno-unused-function")
 
 # Dear GCC, please tell us if a function does not return a value since that part of the standard is stupid!

code/bmpman/bmpman.cpp
@@ -418,7 +418,7 @@ DCF(bmpman, "Shows/changes bitmap caching parameters and usage") {
 	}
 
 	if (dc_optional_string_either("status", "--status") || dc_optional_string_either("?", "--?")) {
-		dc_printf("Total RAM usage: %d bytes\n", bm_texture_ram);
+		dc_printf("Total RAM usage: " SIZE_T_ARG " bytes\n", bm_texture_ram);
 
 		if (Bm_max_ram > 1024 * 1024) {
 			dc_printf("\tMax RAM allowed: %.1f MB\n", i2fl(Bm_max_ram) / (1024.0f*1024.0f));
@@ -434,14 +434,14 @@ DCF(bmpman, "Shows/changes bitmap caching parameters and usage") {
 
 
 	if (dc_optional_string("flush")) {
-		dc_printf("Total RAM usage before flush: %d bytes\n", bm_texture_ram);
+		dc_printf("Total RAM usage before flush: " SIZE_T_ARG " bytes\n", bm_texture_ram);
 		int i;
 		for (i = 0; i < MAX_BITMAPS; i++) {
 			if (bm_bitmaps[i].type != BM_TYPE_NONE) {
 				bm_free_data(i);
 			}
 		}
-		dc_printf("Total RAM after flush: %d bytes\n", bm_texture_ram);
+		dc_printf("Total RAM after flush: " SIZE_T_ARG " bytes\n", bm_texture_ram);
 	} else if (dc_optional_string("ram")) {
 		dc_stuff_int(&Bm_max_ram);
 

code/controlconfig/controlsconfig.cpp
@@ -1974,7 +1974,7 @@ void control_config_do_frame(float frametime)
 		if (t % 2) {
 			gr_set_color_fast(&Color_text_normal);
 			gr_get_string_size(&w, NULL, XSTR( "?", 208));
-			gr_printf_menu(x - w / 2, y - font_height / 2, XSTR( "?", 208));
+			gr_printf_menu(x - w / 2, y - font_height / 2, "%s", XSTR( "?", 208));
 		}
 
 	} else if (!(z & JOY_AXIS) && ((Conflicts[z].key >= 0) || (Conflicts[z].joy >= 0))) {
@@ -1986,7 +1986,7 @@ void control_config_do_frame(float frametime)
 		gr_set_color_fast(&Color_text_normal);
 		str = XSTR( "Control conflicts with:", 209);
 		gr_get_string_size(&w, NULL, str);
-		gr_printf_menu(x - w / 2, y - font_height, str);
+		gr_printf_menu(x - w / 2, y - font_height, "%s", str);
 
 		if (Control_config[i].hasXSTR) {
 			strcpy_s(buf, XSTR(Control_config[i].text, CONTROL_CONFIG_XSTR + i));
@@ -1996,12 +1996,12 @@ void control_config_do_frame(float frametime)
 
 		font::force_fit_string(buf, 255, Conflict_wnd_coords[gr_screen.res][CONTROL_W_COORD]);
 		gr_get_string_size(&w, NULL, buf);
-		gr_printf_menu(x - w / 2, y, buf);
+		gr_printf_menu(x - w / 2, y, "%s", buf);
 
 	} else if (*bound_string) {
 		gr_set_color_fast(&Color_text_normal);
 		gr_get_string_size(&w, NULL, bound_string);
-		gr_printf_menu(x - w / 2, y - font_height / 2, bound_string);
+		gr_printf_menu(x - w / 2, y - font_height / 2, "%s", bound_string);
 		if (timestamp_elapsed(bound_timestamp)) {
 			*bound_string = 0;
 		}
@@ -2009,7 +2009,7 @@ void control_config_do_frame(float frametime)
 
 	if (Cc_lines[Num_cc_lines - 1].y + font_height > Cc_lines[Scroll_offset].y + Control_list_coords[gr_screen.res][CONTROL_H_COORD]) {
 		gr_set_color_fast(&Color_white);
-		gr_printf_menu(Control_more_coords[gr_screen.res][CONTROL_X_COORD], Control_more_coords[gr_screen.res][CONTROL_Y_COORD], XSTR( "More...", 210));
+		gr_printf_menu(Control_more_coords[gr_screen.res][CONTROL_X_COORD], Control_more_coords[gr_screen.res][CONTROL_Y_COORD], "%s", XSTR( "More...", 210));
 	}
 
 	conflict = 0;
@@ -2035,7 +2035,7 @@ void control_config_do_frame(float frametime)
 		if (Cc_lines[line].label) {
 			strcpy_s(buf, Cc_lines[line].label);
 			font::force_fit_string(buf, 255, Control_list_ctrl_w[gr_screen.res]);
-			gr_printf_menu(Control_list_coords[gr_screen.res][CONTROL_X_COORD], y, buf);
+			gr_printf_menu(Control_list_coords[gr_screen.res][CONTROL_X_COORD], y, "%s", buf);
 		}
 
 		if (!(z & JOY_AXIS)) {
@@ -2046,7 +2046,7 @@ void control_config_do_frame(float frametime)
 
 			if ((k < 0) && (j < 0)) {
 				gr_set_color_fast(&Color_grey);
-				gr_printf_menu(x, y, XSTR( "None", 211));
+				gr_printf_menu(x, y, "%s", XSTR( "None", 211));
 
 			} else {
 				if (k >= 0) {
@@ -2066,7 +2066,7 @@ void control_config_do_frame(float frametime)
 						gr_set_color_fast(c);
 					}
 
-					gr_printf_menu(x, y, buf);
+					gr_printf_menu(x, y, "%s", buf);
 
 					Cc_lines[line].kx = x - Control_list_coords[gr_screen.res][CONTROL_X_COORD];
 					gr_get_string_size(&w, NULL, buf);
@@ -2075,7 +2075,7 @@ void control_config_do_frame(float frametime)
 
 					if (j >= 0) {
 						gr_set_color_fast(&Color_text_normal);
-						gr_printf_menu(x, y, XSTR( ", ", 212));
+						gr_printf_menu(x, y, "%s", XSTR( ", ", 212));
 						gr_get_string_size(&w, NULL, XSTR( ", ", 212));
 						x += w;
 					}
@@ -2099,7 +2099,7 @@ void control_config_do_frame(float frametime)
 					}
 
 					font::force_fit_string(buf, 255, Control_list_key_w[gr_screen.res] + Control_list_key_x[gr_screen.res] - x);
-					gr_printf_menu(x, y, buf);
+					gr_printf_menu(x, y, "%s", buf);
 
 					Cc_lines[line].jx = x - Control_list_coords[gr_screen.res][CONTROL_X_COORD];
 					gr_get_string_size(&Cc_lines[line].jw, NULL, buf);
@@ -2115,7 +2115,7 @@ void control_config_do_frame(float frametime)
 
 			if (j < 0) {
 				gr_set_color_fast(&Color_grey);
-				gr_printf_menu(x, y, XSTR( "None", 211));
+				gr_printf_menu(x, y, "%s", XSTR( "None", 211));
 
 			} else {
 				if (Conflicts_axes[z & ~JOY_AXIS] >= 0) {

code/cutscene/cutscenes.cpp
@@ -585,7 +585,7 @@ void cutscenes_screen_do_frame()
 		}
 
 		gr_printf_menu(Cutscene_list_coords[gr_screen.res][0], Cutscene_list_coords[gr_screen.res][1] + y,
-					   Cutscenes[Cutscene_list[z]].name);
+					   "%s", Cutscenes[Cutscene_list[z]].name);
 
 		y += font_height;
 		z++;

code/debugconsole/consoleparse.cpp
@@ -957,7 +957,7 @@ long dc_parse_long(const char *ch, dc_token type) {
 		dc_printf("Error: Could not convert all of the buffer '%s'.\n", buffer_str.c_str());
 		if (Dc_debug_on) {
 			dc_printf("<debug> Buffer value: %s\n", buffer_str.c_str());
-			dc_printf("<debug> Return value: %i", ret);
+			dc_printf("<debug> Return value: %ld", ret);
 		}
 		throw errParse(ch, type);
 	}
@@ -1046,7 +1046,7 @@ ulong dc_parse_ulong(const char *ch, dc_token type) {
 		dc_printf("Error: Could not convert all of the buffer '%s'.\n", buffer_str.c_str());
 		if (Dc_debug_on) {
 			dc_printf("<debug> Buffer value: %s\n", buffer_str.c_str());
-			dc_printf("<debug> Return value: %i", ret);
+			dc_printf("<debug> Return value: %ld", ret);
 		}
 		throw errParse(ch, type);
 	}

code/fs2netd/fs2netd_client.cpp
@@ -141,7 +141,7 @@ void fs2netd_options_config_init()
 		long port_tmp = strtol(Multi_options_g.tracker_port, (char**)NULL, 10);
 
 		if ( (port_tmp < 1024) || (port_tmp > USHRT_MAX) ) {
-			ml_printf("NOTICE: The port specified for game/user trackers, '%i', is outside of the required range, %i through %i!", port_tmp, 1024, USHRT_MAX);
+			ml_printf("NOTICE: The port specified for game/user trackers, '%ld', is outside of the required range, %i through %i!", port_tmp, 1024, USHRT_MAX);
 			ml_printf("NOTICE: Port for game/user trackers is invalid, using default instead (%s).", FS2NETD_DEFAULT_PORT);
 			strncpy( Multi_options_g.tracker_port, FS2NETD_DEFAULT_PORT, STD_NAME_LEN );
 		}
@@ -689,7 +689,7 @@ static void fs2netd_handle_messages()
 					case NETGAME_STATE_IN_MISSION: // gotta make it paused
 						//multi_pause_request(1); 
 						//send_game_chat_packet(Net_player, str, MULTI_MSG_ALL, NULL);
-						HUD_printf(tbuf);
+						HUD_printf("%s", tbuf);
 						break;
 
 					default:

commit 36ac09b54886df195aa02db9e6f76bf8f9c9f6cb

Get rid of duplicate percent signs in the statistics screen.
The problem was caused by an oversight in 07107ce9b208d6dc8c5a2efcb9122b8180908103 (namely, that changing some strings from being passed directly to string-formatting to being passed as arguments to a "%s" format-specifier meant that "%%" was no longer needed to get a plain "%" out the other side).

code/stats/stats.cpp
@@ -53,9 +53,9 @@ void show_stats_label(int stage, int sx, int sy, int dy)
 			sy += dy;
 			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hits", 118));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Primary hit %%", 119));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary hit %", 119));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hit %%", 120));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hit %", 120));
 			sy += 2*dy;
 
 			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary weapon shots", 121));
@@ -64,9 +64,9 @@ void show_stats_label(int stage, int sx, int sy, int dy)
 			sy += dy;
 			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hits", 123));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary hit %%", 124));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary hit %", 124));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hit %%", 125));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hit %", 125));
 			sy += 2*dy;
 
 			gr_printf_menu(sx,sy,"%s", XSTR( "Assists", 126));
@@ -93,9 +93,9 @@ void show_stats_label(int stage, int sx, int sy, int dy)
 			sy += dy;
 			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hits", 118));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Primary hit %%", 119));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary hit %", 119));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hit %%", 120));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Primary friendly hit %", 120));
 			sy += 2*dy;
 
 			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary weapon shots", 121));
@@ -104,9 +104,9 @@ void show_stats_label(int stage, int sx, int sy, int dy)
 			sy += dy;
 			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hits", 123));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary hit %%", 124));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary hit %", 124));
 			sy += dy;
-			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hit %%", 125));
+			gr_printf_menu(sx,sy,"%s", XSTR( "Secondary friendly hit %", 125));
 			sy += 2*dy;			
 
 			gr_printf_menu(sx,sy,"%s", XSTR( "Assists", 126));
@@ -153,13 +153,13 @@ void show_stats_numbers(int stage, int sx, int sy, int dy,int add_mission)
 			if(Active_player->stats.mp_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.mp_shots_hit/(float)Active_player->stats.mp_shots_fired);
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if(Active_player->stats.mp_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.mp_bonehead_hits/(float)Active_player->stats.mp_shots_fired);
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
@@ -176,13 +176,13 @@ void show_stats_numbers(int stage, int sx, int sy, int dy,int add_mission)
 			if(Active_player->stats.ms_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.ms_shots_hit/(float)Active_player->stats.ms_shots_fired);
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if(Active_player->stats.ms_shots_fired>0)
 				pct=(float)100.0*((float)Active_player->stats.ms_bonehead_hits/(float)Active_player->stats.ms_shots_fired);
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
@@ -237,13 +237,13 @@ void show_stats_numbers(int stage, int sx, int sy, int dy,int add_mission)
 			if((Active_player->stats.p_shots_fired + add.p_shots_fired)>0)
 				pct=(float)100.0*((float)(Active_player->stats.p_shots_hit+add.p_shots_hit)/(float)(Active_player->stats.p_shots_fired + add.p_shots_fired));
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if((Active_player->stats.p_bonehead_hits + add.p_bonehead_hits)>0)
 				pct=(float)100.0*((float)(Active_player->stats.p_bonehead_hits+add.p_bonehead_hits)/(float)(Active_player->stats.p_shots_fired + add.p_shots_fired));
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 
@@ -260,13 +260,13 @@ void show_stats_numbers(int stage, int sx, int sy, int dy,int add_mission)
 			if((Active_player->stats.s_shots_fired+add.s_shots_fired)>0)
 				pct=(float)100.0*((float)(Active_player->stats.s_shots_hit + add.s_shots_hit)/(float)(Active_player->stats.s_shots_fired + add.s_shots_fired));
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += dy;
 			if((Active_player->stats.s_bonehead_hits + add.s_bonehead_hits)>0)
 				pct=(float)100.0*((float)(Active_player->stats.s_bonehead_hits+add.s_bonehead_hits)/(float)(Active_player->stats.s_shots_fired+add.s_shots_fired));
 			else pct=(float)0.0;
-			sprintf(text,"%d",(int)pct); strcat_s(text," %%");
+			sprintf(text,"%d",(int)pct); strcat_s(text," %");
 			gr_printf_menu(sx,sy,"%s", text);
 			sy += 2*dy;
 